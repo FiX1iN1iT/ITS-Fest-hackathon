9e777e47f5aeec32bbd855302bf18c7739a2fd05
Mikhail Ulanov <mixaulanoff@yandex.ru>
2024-04-27 02:32:52 +0300

    fix protocols

--- ITSFest/Modules/Base/BaseRouter/BaseRouterProtocol.swift
+++ ITSFest/Modules/Base/BaseRouter/BaseRouterProtocol.swift
@@ -9,7 +9,7 @@ import UIKit
 
 protocol BaseSwiftRouterInput {
     
-    func openModuleFromWindow(window: UIWindow?)
+    func openModuleFromWindow(window: UIWindow?, navigationController: UINavigationController?)
     func openModuleFromViewController(sourseView: UIViewController?, openView: UIViewController)
     func closeModule()
 }
