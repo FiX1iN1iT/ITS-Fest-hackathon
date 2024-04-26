213803526d1268461980278bda2fc159d36c9601
dongrigory <dongrigory29@gmail.com>
2024-04-26 20:19:09 +0300

    Delete coordinator

--- ITSFest/Support/SceneDelegate.swift
+++ ITSFest/Support/SceneDelegate.swift
@@ -10,20 +10,14 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
-    private var coordinator: FlowCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else {
             return
         }
         
-        let navigationController = UINavigationController()
-        coordinator = FlowCoordinator(navController: navigationController)
-        
-        coordinator?.start()
-        
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = navigationController
+//        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 }
